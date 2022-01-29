0e91a8fbc73497e7be08539ba15101b448b87cfb
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-29 23:26:34 +0000

    Build an intent

--- helloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
+++ helloWorld/app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.helloworld
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun sendMessage(view: View){
+        val editText = findViewById<EditText>(R.id.editTextTextPersonName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
     }
 }
