1c632dddfe4c666680c6de910e0acceb57d64d39
tifuseini <tifuseini@st.ug.edu.gh>
2022-01-30 00:39:37 +0000

    Display Activity Update

--- helloWorld/app/src/main/java/com/example/helloworld/DisplayMessageActivity.kt
+++ helloWorld/app/src/main/java/com/example/helloworld/DisplayMessageActivity.kt
@@ -1,2 +1,22 @@
 package com.example.helloworld
 
+import android.os.Bundle
+import android.widget.TextView
+import android.content.Intent
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import androidx.appcompat.app.AppCompatActivity
+
+class DisplayMessageActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        val showMessage = findViewById<TextView>(R.id.showMessage).apply {
+            text = message
+        }
+    }
+
+}
