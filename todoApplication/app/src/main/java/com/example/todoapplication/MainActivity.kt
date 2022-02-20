4f0b829676e3e5ea73dae09f492db61904f3fdd5
tifuseini <tifuseini@st.ug.edu.gh>
2022-02-20 18:09:33 +0000

    fourth Commit

--- todoApplication/app/src/main/java/com/example/todoapplication/MainActivity.kt
+++ todoApplication/app/src/main/java/com/example/todoapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.todoapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var itemlist = arrayListOf<String>()
+        var adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,
+            itemlist)
     }
 }
